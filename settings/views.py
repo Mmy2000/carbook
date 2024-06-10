f44c4192947ccccaa8cf80fefe625b6866717af8
Mmy2000 <yahianada005@gmail.com>
2024-06-10 11:35:47 +0300

    creating home page

--- settings/views.py
+++ settings/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from cars.models import Car
 # Create your views here.
 def home(request):
+    car = Car.objects.all()
     return render(request , 'home.html')
